2a1bb1241bfc1dfc38defefb98ea6ca4321e7e7d
Cristina Rojas Zamora <criss@Cristinas-MacBook-Pro-2.local>
2017-07-13 15:38:26 -0700

    Adding example of setInterval method of window

---
+++ setInterval.js
@@ -0,0 +1,5 @@
+// Method calls a function or evaluates an expression at specified intervals (in milliseconds).
+// The setInterval() method will continue calling the function until clearInterval() is called, or the window is closed.
+setInterval(function(){
+	alert('Hi Criss');
+}, 3000);
